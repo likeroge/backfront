08a199102265df3a81fcdadc4db1e1a302bd0cd5
Egor Puchkov <egor.puchkov@volga-dnepr.com>
2025-09-17 17:43:52 +0400

    add backend

--- back/app.js
+++ back/app.js
@@ -1,13 +1,14 @@
 import express from "express";
 
 const app = express();
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.get("/users", (req, res) => {
-  res.json([
+  res.send([
     { id: 1, name: "John Doe" },
     { id: 2, name: "Jane Doe" },
   ]);
