75883cc1d51b8aeb30eb910927cf956aa905b4c3
Egor Puchkov <egor.puchkov@volga-dnepr.com>
2025-09-18 10:38:17 +0400

    add backend

--- front/src/script.js
+++ front/src/script.js
@@ -1,10 +1,10 @@
 console.log("Hello world!");
 
 async function changeColor() {
-  console.log("changeColor func");
+  console.log("changeColor func2");
 
-  // let data = await fetch("http://backend:5000/users");
-  let data = await fetch("/api/users");
+  let data = await fetch("http://backend:5000/users");
+  // let data = await fetch("/api/users");
 
   console.log(data);
   console.log(data.json());
