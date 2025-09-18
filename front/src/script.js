9da46d447ec3cf3f508bd5f94b7c77ed14c0a9dc
Egor Puchkov <egor.puchkov@volga-dnepr.com>
2025-09-18 12:35:31 +0400

    add backend

--- front/src/script.js
+++ front/src/script.js
@@ -7,7 +7,10 @@ async function changeColor() {
   let data = await fetch("/api/users");
 
   console.log(data);
-  console.log(await data.json());
+  const users = await data.json();
+  console.log(users);
+  const body = await data.body();
+  console.log(body);
 
   const title = document.querySelector(".main-title");
   const colors = ["#ff6b6b", "#4ecdc4", "#45b7d1", "#f9ca24", "#6c5ce7"];
